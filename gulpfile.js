6ba7ae706f4b03dae0dfb5e8b7ecec549053a20d
jean carllo <jeanrosete04@gmail.com>
2023-12-18 15:50:32 -0300

    inserido uglify e imagemin.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const watch = require('gulp-watch');
+const imagemin = require('gulp-image-min');
+const uglify = require('gulp-uglify');
+
 
 function styles () {
     return gulp.src('./src/styles/*.scss')
@@ -8,6 +11,17 @@ function styles () {
     .pipe(gulp.dest('./dist/styles'));
 }
 
+function imagemin () {
+    return gulp.src('./src/image/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function uglify () {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts'));
+}
 
 
 exports.default = gulp.parallel(styles);
